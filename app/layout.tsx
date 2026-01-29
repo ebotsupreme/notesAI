157fdb91b4948eb3b53af38cda23113dd7a285cf
ebotsupreme <ejung716@gmail.com>
2026-01-29 14:13:59 -0800

    Add sidebar component. Adjust background colors of sidebar, header and body.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Toaster } from "@/components/ui/sonner";
 import type { Metadata } from "next";
 import Header from "@/components/Header";
 import "@/styles/globals.css";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/AppSidebar";
 
 export const metadata: Metadata = {
   title: "Notes AI",
@@ -22,12 +24,16 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <div className="flex min-h-screen w-full flex-col">
-            <Header />
-            <main className="flex flex-1 flex-col px-4 pt-10 xl:px-8">
-              {children}
-            </main>
-          </div>
+          <SidebarProvider>
+            <AppSidebar />
+            <div className="flex min-h-screen w-full flex-col">
+              <Header />
+              <main className="flex flex-1 flex-col px-4 pt-10 xl:px-8">
+                <SidebarTrigger />
+                {children}
+              </main>
+            </div>
+          </SidebarProvider>
           <Toaster richColors />
         </ThemeProvider>
       </body>
