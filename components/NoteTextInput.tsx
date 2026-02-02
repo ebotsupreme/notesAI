101375f9041b7be1f350b7af6fc3186803e2b708
ebotsupreme <ejung716@gmail.com>
2026-02-02 13:32:12 -0800

    Create NoteProviderContext

--- components/NoteTextInput.tsx
+++ components/NoteTextInput.tsx
@@ -1,13 +1,45 @@
 "use client";
 
+import { useSearchParams } from "next/navigation";
+import { Textarea } from "./ui/textarea";
+import { ChangeEvent, useEffect } from "react";
+import { DEBOUNCE_TIMEOUT } from "@/lib/constants";
+
 type Props = {
   noteId: string;
   startingNoteText: string;
 };
 
+let updateTimeout: NodeJS.Timeout; // for debounce
+
 function NoteTextInput({ noteId, startingNoteText }: Props) {
-  console.log("NoteTextInput:", { noteId, startingNoteText });
-  return <div>Note Text Input</div>;
+  const noteIdParam = useSearchParams().get("noteId");
+  const { noteText, setNoteText } = useNote();
+
+  useEffect(() => {
+    if (noteIdParam === noteId) {
+      setNoteText(startingNoteText);
+    }
+  }, [startingNoteText, noteIdParam, noteId, setNoteText]);
+
+  const handleUpdateNote = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    const text = e.target.value;
+    setNoteText(text);
+
+    clearTimeout(updateTimeout);
+    updateTimeout = setTimeout(() => {
+      updateNoteAction(noteId, text);
+    }, DEBOUNCE_TIMEOUT);
+  };
+
+  return (
+    <Textarea
+      value={noteText}
+      onChange={handleUpdateNote}
+      placeholder="Type your notes here..."
+      className="custom-scrollbar placeholder:text-muted-foreground focus:visible:ring-offset-0 mb-4 h-full max-w-4xl resize-none border p-4 focus-visible:ring-0"
+    />
+  );
 }
 
 export default NoteTextInput;
